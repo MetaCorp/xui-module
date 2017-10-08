99b3eb3366c74ddd45510264461eb314c3a1d56c
Meta <l.szabatura@gmail.com>
2017-10-08 17:15:06 +0200

    Add dependency

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
-// const { resolve } = require('path')
+const { resolve } = require('path')
 
-export default async function module (moduleOptions) {
-  // const options = Object.assign({}, moduleOptions)
+const defaultOptions = {
+  'bulma': {
+    active: true
+  },
+  'mdi': {
+    active: true
+  },
+  'animate.css': {
+    active: true
+  },
+  'vue-in-viewport-directive': {
+    active: true
+  },
+  'vue-scrollactive': {
+    active: true
+  },
+  'vue-headroom': {
+    active: true,
+    options: { ssr: false }
+  },
+}
 
-  // this.addPlugin({
-  //   src: resolve(__dirname, '../templates/plugin.js'),
-  //   options
-  // })
+export default function module (moduleOptions) {
+  const options = Object.assign(defaultOptions, moduleOptions)
+
+  options['bulma'].active && this.options.css.push('bulma')
+  options['mdi'].active && this.options.css.push('mdi/css/materialdesignicons.min.css')
+  options['animate.css'].active && this.options.css.push('animate.css')
+
+  options['vue-in-viewport-directive'].active && this.addPlugin({
+    src: resolve(__dirname, '../plugins/vue-in-viewport-directive.js'),
+    options: options['vue-in-viewport-directive'].options
+  })
+
+  options['vue-scrollactive'].active && this.addPlugin({
+    src: resolve(__dirname, '../plugins/vue-scrollactive.js'),
+    options: options['vue-scrollactive'].options
+  })
+
+  options['vue-headroom'].active && this.addPlugin({
+    src: resolve(__dirname, '../plugins/vue-headroom.js'),
+    options: options['vue-headroom'].options
+  })
 }
