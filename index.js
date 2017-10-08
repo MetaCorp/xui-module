31cc90d2284e88361ce479c3096a0be9f27757db
Meta <l.szabatura@gmail.com>
2017-10-08 19:31:34 +0200

    Clean project

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const defaultOptions = {
   },
 }
 
-module.exports = function module (moduleOptions) {
+export default function xui (moduleOptions) {
   const options = Object.assign(defaultOptions, moduleOptions)
 
   options['bulma'].active && this.options.css.push('bulma')
@@ -30,19 +30,19 @@ module.exports = function module (moduleOptions) {
   options['animate.css'].active && this.options.css.push('animate.css')
 
   options['vue-in-viewport-directive'].active && this.addPlugin({
-    src: resolve(__dirname, '../plugins/vue-in-viewport-directive.js'),
+    src: resolve(__dirname, './plugins/vue-in-viewport-directive.js'),
     options: options['vue-in-viewport-directive'].options
   })
 
   options['vue-scrollactive'].active && this.addPlugin({
-    src: resolve(__dirname, '../plugins/vue-scrollactive.js'),
+    src: resolve(__dirname, './plugins/vue-scrollactive.js'),
     options: options['vue-scrollactive'].options
   })
 
   options['vue-headroom'].active && this.addPlugin({
-    src: resolve(__dirname, '../plugins/vue-headroom.js'),
+    src: resolve(__dirname, './plugins/vue-headroom.js'),
     options: options['vue-headroom'].options
   })
 }
 
-module.exports.meta = require('../package.json')
+module.exports.meta = require('./package.json')
